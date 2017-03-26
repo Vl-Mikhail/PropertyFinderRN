f60bcd31c727bba661ca540a0a29ee47e3710446
Vl-Mikhail <evil_bear@mail.ru>
2017-03-26 21:34:50 +0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    NavigatorIOS,
+} from 'react-native';
+import SearchPage from "./SearchPage";
+
+/**
+ *
+ */
+class PropertyFinderRN extends React.Component {
+    render () {
+        return (
+            <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                    title: 'Property Finder',
+                    component: SearchPage,
+                }}/>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'black',
+        backgroundColor: 'white',
+        fontSize: 30,
+        margin: 80
+    },
+    container: {
+        flex: 1
+    },
+});
+
+AppRegistry.registerComponent('PropertyFinderRN', () => PropertyFinderRN);
